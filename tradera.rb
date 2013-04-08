841e067c0f32042d8c2adb325ede307a284579d8
Christoffer Ekeroth <christoffer@campanja.com>
2013-04-08 15:31:56 +0200

    Add Tradera module

---
+++ tradera.rb
@@ -0,0 +1,37 @@
+require 'open-uri'
+require 'savon'
+
+class Tradera
+
+  # Credentials for Campanja test app
+  AppId = ENV['APP_ID']
+  ServiceKey = ENV['SERVICE_KEY']
+  PublicKey = ENV['PUBLIC_KEY']
+  Domain = ENV['DOMAIN']
+
+  def initialize
+    service = 'v3/PublicService.asmx'
+    @client = Savon.client do
+      log false
+      wsdl "#{Domain}/#{service}?WSDL"
+      endpoint "#{Domain}/#{service}?appId=#{AppId}&appKey=#{ServiceKey}"
+    end
+  end
+
+  def get_categories
+    response = @client.call(:get_categories)
+    response.body[:get_categories_response][:get_categories_result]
+  end
+
+  def get_seller_items(user_id)
+    message = {
+      userId: user_id,
+      categoryId: 0,
+      filterType: 0,
+      minEndDate: nil,
+      maxEndDate: nil,
+    }
+    response = @client.call(:get_seller_items, message: message)
+    response.body[:get_seller_items_response][:get_seller_items_result]
+  end
+end
