17e0598a7f6afbd337383411b6efc540a796cb62
Christoffer Ekeroth <christoffer@campanja.com>
2013-04-08 16:31:20 +0200

    Add an items route for reading items from Tradera

--- web.rb
+++ web.rb
@@ -1,5 +1,25 @@
+require 'json'
+# External libs
 require 'sinatra'
+# Internal modules
+require 'tradera.rb'
 
 get '/' do
   redirect '/index.html'
+end
+
+get '/users/:id/items' do
+  c = Tradera.new
+  response = c.get_seller_items params[:id]
+  products = response[:item].map do |x|
+    {
+      "headline" => x[:short_description],
+      "productImage" => x[:thumbnail_link],
+      "currentBid" => x[:max_bid],
+      "hoursLeft" => 0,
+      "minutesLeft" => 0,
+    }
+  end
+  content_type 'application/json'
+  {"products" => products}.to_json
 end
