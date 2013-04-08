f3a89234bd264f6df6720d69af7b4dea4b2a0c09
Samir Fors <samir.fors@gmail.com>
2013-04-08 15:40:52 +0200

    [wip] social sharing.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -14,7 +14,6 @@ var CharityApp = (function() {
               template = Handlebars.compile(source),
               self = this;
         _.each(data.products, function(product) {
-          console.log(product)
           var context = {
               headline: product.headline,
               productImage: product.productImage,
@@ -24,15 +23,32 @@ var CharityApp = (function() {
             },
             html = template(context);
           $('.auction-columns').append(html);
-        })
+        });
+
+        $('.share-on-facebook').on('click', shareOnFacebook);
+
+        $('.share-on-twitter').on('click', shareOnTwitter);
       },
       error: function(xhr, errorType, error) {
         console.log(['now there was error', xhr, errorType, error]);
       }
     });
-  }
+  };
+
+  var shareOnFacebook = function(e) {
+    e.preventDefault();
+    console.log('http://www.facebook.com/sharer.php?u=http://localhost:8080');
+  };
+
+  var shareOnTwitter = function(e) {
+    e.preventDefault();
+    console.log("http://twitter.com/share?text=Support%20charity!%20And%20buy%20new%20things!&url=http://localhost:8080");
+  };
 
   return {
     getProducts: getProducts
   };
 })();
+
+CharityApp.getProducts();
+
